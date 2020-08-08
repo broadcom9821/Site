430470bae76a18dfe9b395981c26cf2ad70e3701
broadcom9821 <44141692+broadcom9821@users.noreply.github.com>
2020-08-08 14:45:14 +0300

    Initial commit

--- PenpineappleapplePen/views.py
+++ PenpineappleapplePen/views.py
@@ -4,8 +4,19 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request,'app/index.html')
+
+def about(request):
+	return render(request, 'app/About/about.html')
 def contact(request):
-	return render(request,'app/contact.html')
+	return render(request, 'app/About/contact.html')
+def Gallery(request):
+	return render(request, 'app/About/Gallery.html')
+def History(request):
+	return render(request, 'app/About/History.html')
+def SMI(request):
+	return render(request, 'app/About/SMI.html')
+def Team(request):
+	return render(request, 'app/About/Team.html')
 def fullwidth(request):
 	return render(request,'app/full-width.html')
 def sidebar(request):
@@ -14,7 +25,6 @@ def faq(request):
 	return render(request,'app/faq.html')
 def error(request):
 	return render(request,'app/404.html')
-def about(request):
-	return render(request,'app/about.html')
+
 def services(request):
 	return render(request,'app/services.html')
