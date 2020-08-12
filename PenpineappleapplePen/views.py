ed7987e52639890d9b263e0a5df2469e49890a1d
broadcom9821 <44141692+broadcom9821@users.noreply.github.com>
2020-08-12 23:47:17 +0300

    Initial commit

--- PenpineappleapplePen/views.py
+++ PenpineappleapplePen/views.py
@@ -6,15 +6,15 @@ def index(request):
 	return render(request,'app/index.html')
 
 def EGE_I_GIA(request):
-	return render(request,'app/About/EGE_I_GIA.html')
+	return render(request, 'app/Education/EGE_I_GIA.html')
 def Mattex_obesp(request):
-	return render(request,'app/About/Mattex_obesp.html')
+	return render(request, 'app/Education/Mattex_obesp.html')
 def Obr_model(request):
-	return render(request,'app/About/Obr_model.html')
+	return render(request, 'app/Education/Obr_model.html')
 def Obr_stand(request):
-	return render(request,'app/About/Obr_stand.html')
+	return render(request, 'app/Education/Obr_stand.html')
 def Dop_obraz(request):
-	return render(request,'app/About/Dop_obraz.html')
+	return render(request, 'app/Education/Dop_obraz.html')
 def about(request):
 	return render(request, 'app/About/about.html')
 def contact(request):
